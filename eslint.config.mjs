c769257636535a438b22ece08c8e6d61ffa28f1f
Sumca <1095028691@qq.com>
2024-06-27 17:05:19 +0800

    fix:修改eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,11 +2,20 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginVue from "eslint-plugin-vue";
+import tsParser from '@typescript-eslint/parser';
 
 
 export default [
   {
-    languageOptions: { globals: globals.browser },
+    languageOptions: {
+      // 使用 TypeScript 解析器
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 'latest', // ECMAScript 最新版本
+        sourceType: 'module', // ECMAScript 模块
+      },
+      globals: globals.browser 
+    },
   },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
@@ -17,6 +26,11 @@ export default [
   {
     rules: {
       // "@typescript-eslint/no-var-requires": "off"
-    }
+    },
+    extends: [
+      'eslint:recommended', // 使用 ESLint 推荐规则
+      'plugin:@typescript-eslint/recommended', // 使用 @typescript-eslint 推荐规则
+      'plugin:vue/vue3-recommended', // 使用 Vue 3 推荐规则
+    ],
   },
 ];
